9adad551158b1edcd989e7a0e46e5611b23019f6
Sardius <0xsardius@gmail.com>
2024-09-02 20:47:58 -0400

    test and helperconfig update

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,8 +3,16 @@
 pragma solidity 0.8.19;
 
 import {Script}  from "forge-std/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract CodeConstants {
+    // VRF Mock Values
+    uint96 public MOCK_BASE_FEE = 0.25 ether;
+    uint96 public MOCK_GAS_PRICE_LINK = 1e9;
+    // Link / ETH Price
+    int256 public MOCK_WEI_PER_UINT_LINK = 4e15;
+
+
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
@@ -33,9 +41,14 @@ contract HelperConfig is Script, CodeConstants {
            return networkConfigs[chainId];
        } else if (chainId == LOCAL_CHAIN_ID) {
            // getOrCreateAnvilEthConfig
+              return getOrCreateAnvilEthConfig();
        } else {
               revert HelperConfig__InvalidChainId();
        }
+    }
+
+    function getConfig() public returns(NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
     }
 
     function getSepoliaEthConfig() public pure returns(NetworkConfig memory) {
@@ -52,7 +65,23 @@ contract HelperConfig is Script, CodeConstants {
     function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory) {
         // check to see if we set an active network Config
         if (localNetworkConfig.vrfCoordinator != address(0)) {
-            return
+            return localNetworkConfig;
         }
+
+
+        // Deploy mocks
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK, MOCK_WEI_PER_UINT_LINK);
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 0.01 ether, // 1e16
+            interval: 30, //30 seconds
+            vrfCoordinator:  0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            callbackGasLimit: 500000, // 500,000 gas
+            subscriptionId: 0
+        });
+        return localNetworkConfig;
     }
 }
